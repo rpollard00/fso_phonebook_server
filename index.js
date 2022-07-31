f34746f5f7d5ef38aa06b35e154ea22bc7d033b1
Reese Pollard <rpollard@gmail.com>
2022-07-31 13:10:46 -0700

    Implement 3.3 and 3.4

--- index.js
+++ index.js
@@ -34,11 +34,24 @@ app.get ('/api/persons', (request, response) => {
   response.json(persons)
 })
 
+app.get ('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => id === person.id)
+
+  person ? response.json(person) : response.status(404).end()
+})
+
 app.get ('/info', (request, response) => {
   response.send(`
     <p>Phonebook has info for ${persons.length} people</p>
     <p>${new Date()}</p>
   `)
+})
+
+app.delete ('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
 })
 
 const PORT = 3001;
