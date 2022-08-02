d7ff2fa9abbf6fbe378e024d3ab2aaad620fda36
Reese Pollard <rpollard@gmail.com>
2022-08-01 21:33:53 -0700

    Implement 3.15

--- index.js
+++ index.js
@@ -86,9 +86,11 @@ app.post ('/api/persons', (request, response) => {
 // })
 
 app.delete ('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.get ('/info', (request, response) => {
@@ -97,6 +99,26 @@ app.get ('/info', (request, response) => {
     <p>${new Date()}</p>
   `)
 })
+
+// last middleware
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+
+}
+
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    response.status(400).send({ error: 'malformed id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT);
