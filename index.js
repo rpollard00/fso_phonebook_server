9fbd1e4d54cf6f09e197ed68b00b02c378a817fe
Reese Pollard <rpollard@gmail.com>
2022-08-01 21:05:09 -0700

    Add mongoDB, exercises 3.13 and 3.14

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
+require('dotenv').config()
 const morgan = require('morgan')
+const Person = require('./models/person')
 const cors = require('cors')
 const app = express()
 
@@ -10,36 +12,14 @@ app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
 
-
-let persons = [
-    {
-      "id": 1,
-      "name": "Julio Rodriguez",
-      "phone": "555-219-4200"
-    },
-    {
-      "id": 2,
-      "name": "Jesse Winker",
-      "phone": "360-867-5309"
-    },
-    {
-      "id": 3,
-      "name": "Ty France",
-      "phone": "1-800-TYF-RANC"
-    },
-    {
-      "id": 4,
-      "name": "Cal Raleigh",
-      "phone": "1-800-BIG-Dump"
-    }
-]
-
 app.get('/', (request, response) => {
   response.send('<h1>Helloo world!</h1>')
 })
 
 app.get ('/api/persons', (request, response) => {
-  response.json(persons)
+  Person.find({}).then(persons => {
+    response.json(persons)
+  })
 })
 
 app.get ('/api/persons/:id', (request, response) => {
@@ -55,42 +35,55 @@ const generateRandomId = () => {
 
 app.post ('/api/persons', (request, response) => {
   const body = request.body
-  // generate a random id
-  // create a person object
-  const person = {
-    id: generateRandomId(),
+
+  const person = new Person({
     name: body.name,
-    phone: body.phone
-  }
+    phone: body.phone,
+  })
 
-  if (!body.name) {
-    return response.status(400).json({
-      'error': 'missing name'
-    })
-  } else if (!body.phone) {
-    return response.status(400).json({
-      'error': 'missing phone number'
-    })
-  }
-
-  if (persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
-    return response.status(409).json({ 
-      'error': `Name ${body.name} already exists`
-    })
-  } else if (persons.find(p => Number(p.id) === person.id)) {
-    return response.status(409).json({
-      'error': `duplicate id generated`
-    })
-  }
-
-  persons = persons.concat(person)
-  //res.send(`${person}`)
-  response.status(200).json(person)
-  // add the person to the array
-
-  // fail if the name is duplicate or id is duplicate
-
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
 })
+
+// app.post ('/api/persons', (request, response) => {
+//   const body = request.body
+//   // generate a random id
+//   // create a person object
+//   const person = {
+//     id: generateRandomId(),
+//     name: body.name,
+//     phone: body.phone
+//   }
+
+//   if (!body.name) {
+//     return response.status(400).json({
+//       'error': 'missing name'
+//     })
+//   } else if (!body.phone) {
+//     return response.status(400).json({
+//       'error': 'missing phone number'
+//     })
+//   }
+
+//   if (persons.find(p => p.name.toLowerCase() === body.name.toLowerCase())) {
+//     return response.status(409).json({ 
+//       'error': `Name ${body.name} already exists`
+//     })
+//   } else if (persons.find(p => Number(p.id) === person.id)) {
+//     return response.status(409).json({
+//       'error': `duplicate id generated`
+//     })
+//   }
+
+//   persons = persons.concat(person)
+//   //res.send(`${person}`)
+//   response.status(200).json(person)
+//   // add the person to the array
+
+//   // fail if the name is duplicate or id is duplicate
+
+// })
 
 app.delete ('/api/persons/:id', (request, response) => {
   const id = Number(request.params.id)
